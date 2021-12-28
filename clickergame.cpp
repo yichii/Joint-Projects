3e3e99ffff9537d6aef34079f47372d87db21499
yichiii <55825777+yichii@users.noreply.github.com>
2021-12-28 00:00:14 -0800

    fresh start

---
+++ clickergame.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <conio.h>
+#include <windows.h>
+
+using namespace std;
+
+int main(){
+    while (1){
+        if (GetAsyncKeyState(0x01)){
+            cout << "Left Mkey";
+            Sleep(100);
+        } else if (GetAsyncKeyState(0x09))
+            break;
+    }
+
+    getch();
+
+    return 0;                                                                                               
+}
